886d35928c44ff52987c5b021a45f1be716162e5
Nana Kofi Okae <89530363+Nan3rOkae@users.noreply.github.com>
2024-10-11 20:13:57 -0400

    added the mapbox map to the project

---
+++ components/mapbox.tsx
@@ -0,0 +1,31 @@
+import React, { useEffect, useRef } from "react";
+import mapboxgl from "mapbox-gl";
+import config from "@/config/config";
+
+mapboxgl.accessToken = config.accessToken as string;
+
+const MapboxMap = () => {
+  const mapContainerRef = useRef(null);
+
+  useEffect(() => {
+    if (!mapContainerRef.current) return;
+
+    const map = new mapboxgl.Map({
+      container: mapContainerRef.current,
+      style: config.style as string,
+      center: [10.4515, 51.1657],
+      zoom: 5,
+      projection: "mercator",
+    });
+
+    return () => {
+      map.remove();
+    };
+  }, []);
+
+  return (
+    <div ref={mapContainerRef} style={{ height: "100%", width: "100%" }} />
+  );
+};
+
+export default MapboxMap;
