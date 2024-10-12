29538358a03c019f5a025979000e5c86e71e9341
Nana Kofi Okae <89530363+Nan3rOkae@users.noreply.github.com>
2024-10-11 21:19:10 -0400

    map toggle button added

--- app/page.tsx
+++ app/page.tsx
@@ -1,22 +1,39 @@
 "use client";
-import React from "react";
-import MapboxMap from "@/components/mapbox"; // Adjust the import path accordingly
+import React, { useState } from "react";
+import MapboxMap from "@/components/ui/mapbox";
+import { Button } from "@/components/ui/button";
+import { Map } from "lucide-react";
 
 export default function Home() {
+  const [isMapOpen, setIsMapOpen] = useState(false);
+
+  const toggleMapView = () => {
+    setIsMapOpen(!isMapOpen);
+  };
+
   return (
     <div
-      className="flex-1 md:mt-[40px] mt-[50px] flex gap-4 max-md:flex-col"
+      className="flex-1 md:mt-[40px] mt-[50px] flex flex-col gap-1"
       style={{ height: "calc(100% - 62px)" }}
     >
-      {/* This is where the controls for everything including the content data is going to go */}
-      <div className="border w-full max-sm:w-full flex flex-col gap-2">
-        <div>controls</div>
-        <div>Main content</div>
+      <div className=" controls-container p-1  flex justify-between items-center">
+        <div className="dynamic-control-buttons">dynamic controls</div>
+        <Button variant="outline" onClick={toggleMapView}>
+          <Map size={15} className="mr-1" />
+          Map view
+        </Button>
       </div>
-
-      {/* Mapbox canvas is going to be located in this area */}
-      <div className="border w-full rounded-xl max-sm:w-full h-[500px] md:h-full overflow-hidden">
-        <MapboxMap />
+      <div className="flex max-md:flex-col h-full gap-2">
+        <div className=" w-full max-sm:w-full flex flex-col gap-2">
+          <div className="border h-full">Main content page</div>
+        </div>
+        {isMapOpen ? (
+          <></>
+        ) : (
+          <div className=" map-view-container w-full  max-sm:w-full h-[500px] md:h-full overflow-hidden">
+            <MapboxMap />
+          </div>
+        )}
       </div>
     </div>
   );
