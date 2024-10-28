8f46b9b6926753394c93a5d5b04bfe32ed626842
Nana Kofi Okae <89530363+Nan3rOkae@users.noreply.github.com>
2024-10-28 16:05:09 -0400

    fixing something

--- app/poi/page.tsx
+++ app/poi/page.tsx
@@ -1,12 +1,8 @@
 "use client";
 
 import React from "react";
-import FeatureDetails from "@/components/ui/FeatureDetails";
-import { useFeature } from "@/context/FeatureContext";
 
 export default function PoiPage() {
-  const { selectedFeature } = useFeature();
-
   return (
     <div className="flex flex-col md:flex-row h-full">
       <div className="w-full  p-4 overflow-auto">
@@ -15,11 +11,6 @@ export default function PoiPage() {
           This page contains information about various points of interest
           related to Nazi war crimes.
         </p>
-        {selectedFeature ? (
-          <FeatureDetails />
-        ) : (
-          <div>Click on a point on the map to see details.</div>
-        )}
       </div>
     </div>
   );
